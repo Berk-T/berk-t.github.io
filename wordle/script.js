f032f985a741b719c9ab90685a4d479d51a8dde1
Berk Takıt <berk.takit@tum.de>
2024-11-17 18:50:21 +0100

    Fix Video Not Loading

--- wordle/script.js
+++ wordle/script.js
@@ -15471,7 +15471,7 @@ async function checkWinLose(guess, tiles) {
         document.body.style.animation = "fadeOut 3s";
         await new Promise((r) => setTimeout(r, 2900));
         document.body.innerHTML =
-            "<video id='expanding-screen' autoplay='true' src='bensu.mp4' type='video/mp4'></video>";
+            '<video id="expanding-screen" src="https://drive.google.com/file/d/1rEzwVmmobl0E8mUu29jqH3tSxNv_UT94/preview" width="640" height="480" allow="autoplay"></video>';
         return;
     }
 
